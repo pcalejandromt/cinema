fc7f69886550786cf253ed8ebb67a35ee978be19
pcalejandromt <alejandro.perez.mt@usa.edu.co>
2022-10-10 23:32:21 -0500

    nuevos metodos Put y delete

--- src/main/java/com/cinema/controller/CategoryController.java
+++ src/main/java/com/cinema/controller/CategoryController.java
@@ -8,9 +8,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,4 +40,16 @@ public class CategoryController {
     public Optional<Category> getCategory(@PathVariable("id") int Id){
         return categoryService.getCategory(Id);        
     }
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category update(@RequestBody Category c){
+        return categoryService.update(c);
+    }
+    
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Boolean  deleteCategory(@PathVariable("id") int id){
+        return categoryService.deleteCategory(id);  
+    }    
+    
 }
--- src/main/java/com/cinema/service/CategoryService.java
+++ src/main/java/com/cinema/service/CategoryService.java
@@ -1,4 +1,3 @@
-
 package com.cinema.service;
 
 import com.cinema.model.Category;
@@ -10,27 +9,58 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
     @Autowired
     private CategoryRepository categoryRepository;
-    
-     public List<Category> getAll(){
-      return categoryRepository.getAll();
+
+    public List<Category> getAll() {
+        return categoryRepository.getAll();
+    }
+
+    public Optional<Category> getCategory(int id) {
+        return categoryRepository.getCategory(id);
     }
     
-     public Optional<Category> getCategory(int id){
-         return categoryRepository.getCategory(id);
-     }
-     
-     public Category save(Category c){
-         if(c.getId()==null){
-             return categoryRepository.save(c);
-         }else{
-             Optional ce = categoryRepository.getCategory(c.getId());
-             if(ce.isPresent()){
-                return c;
+    public boolean deleteCategory(int id){
+       Optional<Category> oc=getCategory(id);
+       if(!oc.isEmpty()){
+           categoryRepository.delete(oc.get());
+           return true;
+       }
+       return false;
+    }
+    public Category update(Category c){
+        if (c.getId()!=null){
+            Optional<Category> ca = categoryRepository.getCategory(c.getId());
+            if (!ca.isEmpty()){
+                if(c.getName()!=null){
+                    ca.get().setName(c.getName());
+                }
+                if(c.getDescription()!=null){
+                    ca.get().setDescription(c.getDescription());
+                }
+                if(c.getCinemas()!=null){
+                    ca.get().setCinemas(c.getCinemas());
+                }
+                categoryRepository.save(ca.get());
+                return ca.get();
             }else{
+                return c;
+            }
+        }else{
+            return c;
+        }
+    }
+    public Category save(Category c) {
+        if (c.getId() == null) {
+            return categoryRepository.save(c);
+        } else {
+            Optional ce = categoryRepository.getCategory(c.getId());
+            if (ce.isPresent()) {
+                return c;
+            } else {
                 return categoryRepository.save(c);
-            }  
-         }
-     }
+            }
+        }
+    }
 }
